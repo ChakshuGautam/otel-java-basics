46dc2737e6f2a748912cf758d21193d638c520bf
tedsuo <ted@lightstep.com>
2020-12-04 02:02:14 -0800

    more comments

--- client/src/main/java/com/lightstep/examples/client/App.java
+++ client/src/main/java/com/lightstep/examples/client/App.java
@@ -12,14 +12,20 @@ import okhttp3.Response;
 
 public class App
 {
+  // Create a tracer and name it after your package.
+  // This tags all data created with this tracer as
+  // coming from this package.
   private static final Tracer tracer =
       OpenTelemetry.getGlobalTracer("com.lightstep.examples.client.App");
 
   public static void main( String[] args )
       throws Exception
     {
+      // Create a new span to start a trace.
       Span span = tracer.spanBuilder("main").startSpan();
-      
+
+      // Set the span as active. Spans within this scope
+      // will automatically become children when created.
       try (Scope scope = span.makeCurrent()) {
         // create five requests. notice that the 
         // requests are linked together in the trace 
@@ -38,7 +44,8 @@ public class App
       }
     }
 
-  // annotations are an alternative to making spans by hand
+  // Annotations are an alternative to making spans by hand.
+  // WithSpan wraps a method, measuring it's duration.
   @WithSpan(value="make-request")
   static void makeRequest()
   {
