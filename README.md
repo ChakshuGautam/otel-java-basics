fb8d3fa32ff4c7a44a9ee609eab11f807c45d99a
tedsuo <ted@lightstep.com>
2021-02-01 14:48:00 -0800

    Add spring to readme, improve API comments

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # OpenTelemetry Java Basics
 
-Just a basic client/server for playing with OpenTelemetry in Java.
+This is a basic client/server for playing with OpenTelemetry in Java and learning all of the basic API calls. The code is very simple and heavily commented; it makes for a good source if you are looking for a place to copypasta OpenTelemetry patterns from.
 
 Handy links:
 
-* Free Lightstep Developer Account: https://bit.ly/otel-workshop
+* Get a [Free Lightstep Developer Account](https://app.lightstep.com/signup/developer?signup_source=tyjavabasics). For this example, a Lightstep account is needed to see your data.
 * OTel Java Launcher: https://github.com/lightstep/otel-launcher-java/
 * Quickstart Guide: https://opentelemetry.lightstep.com/java
 * OpenTelemetry Java: https://github.com/open-telemetry/opentelemetry-java
 
-Define your Lightstep Access Token prior to run:
+## Setup
+Define your Lightstep Access Token prior to running the app. You can find your access token under "settings" in the Lightstep UI.
 
 ```sh
-export LS_ACCESS_TOKEN=my-access-token-etc
+export LS_ACCESS_TOKEN=<YOUR ACCESS TOKEN GOES HERE>
 ```
 
 Build:
@@ -32,3 +33,68 @@ Run the client to perform a few requests:
 ```sh
 make run-client
 ```
+
+
+## Auto-instrumentation: Java Spring example
+
+The code in this repo shows how to decorate a simple Servlet with OpenTelemetry
+calls to teach you how to manually instrument your code. But if you are using a 
+supported framework like spring, you get a lot of auto-instrumentation out of the box.
+
+Let's take (Pet Clinic)[https://github.com/spring-projects/spring-petclinic], an
+example Spring app, and add OpenTelemetry to it.
+
+First, set up Petclinic:
+
+```sh
+git clone https://github.com/spring-projects/spring-petclinic.git
+cd spring-petclinic
+./mvnw package
+```
+
+Then, fetch the Lightstep Launcher for Java. The Launcher is a distro of the
+OpenTelemetry Java Agent that is pre-configured to talk to Lightstep:
+
+```sh
+curl -L -O https://github.com/lightstep/otel-launcher-java/releases/download/0.14.0/lightstep-opentelemetry-javaagent-0.14.0.jar
+```
+
+Use env vars to add your Lightstep access token and to correctly name your service.
+
+```sh
+export LS_ACCESS_TOKEN=<YOUR ACCESS TOKEN GOES HERE>
+export LS_SERVICE_NAME=petclinic
+```
+
+With that, you're ready to start petclinic locally, with the Launcher attached as 
+a Java agent.
+
+```sh
+java -javaagent:lightstep-opentelemetry-javaagent-0.14.0.jar -jar target/*.jar
+```
+
+After petclinic starts, go to [http://localhost:8080/](http://localhost:8080/) and
+interact with it a bit.
+
+* [Add an Owner](http://localhost:8080/owners/new)
+* [Add a Pet](http://localhost:8080/owners/1)
+* [Find Owners and edit them](http://localhost:8080/owners/find)
+* [Create some errors](http://localhost:8080/oups)
+
+After you've messed around a bit, have a look at the data by clicking on the
+Explorer in Lightstep. You'll see a number of spans; click on any of them to see
+a trace.
+
+Notice how much information is automatically available:
+
+![Trace of the Petclinic application](petclinic_trace.png)
+
+### Exercise
+All of this out of the box information is great, but you can decorate it further by using the OpenTelemetry API. Using the example code in this repo as a guide, try to do the following:
+
+* Add the first and last name of the owner to the `OwnerController.showOwner` span as attributes. Notice that you can now search by these attributes in Lightstep.
+* When the new owner form is not filled out correctly, set the status of the `OwnerController.processCreationForm` span to ERROR. Notice that errors now appear in Lightstep when you submit a new owner form without filling in all the fields.
+* Add the owner name to other operations that deal with owners. Write a helper method on the Owner model that adds this information to the current span, so that you can adjust what owner attributes are added in a centralized place. Use the helper method in your controllers.
+* How can might you use Spring features, such as interceptors, to help manage OpenTelemetry? For example, how would you add more span attributes to your controllers without having to put them directly in your application code?  
+
+Thanks for checking out this tutorial! Any questions, [please start a discussion](https://github.com/tedsuo/otel-java-basics/discussions/). Bugs or other problems? Please [file an issue](https://github.com/tedsuo/otel-java-basics/issues). Hope it was helpful. :)
--- server/src/main/java/com/lightstep/examples/server/ApiContextHandler.java
+++ server/src/main/java/com/lightstep/examples/server/ApiContextHandler.java
@@ -29,17 +29,23 @@ public class ApiContextHandler extends ServletContextHandler
     addServlet(new ServletHolder(new ApiServlet()), "/hello");
   }
 
+  // HttpServlet is automatically instrumented by OpenTelemetry
   static final class ApiServlet extends HttpServlet
   {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res)
       throws ServletException, IOException
     {
-      // the current span has automatically been created by 
-      // the servlet instrumentation
+      // The current span has automatically been created by 
+      // the servlet instrumentation.
       Span span = Span.current();
       
-      // define the route name using semantic conventions
+      // Set attributes to describe your operation. Attributes 
+      // also function as indexes for finding your spans later. 
+      // In this case, we're adding a route name, which is a common attribute.
+      // These common attributes are defined using semantic conventions. Use these
+      // conventions whenever they are available, as it helps backends recognize 
+      // the meaning of the data.
       // https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/semantic_conventions/http.md#http-server-semantic-conventions
       span.setAttribute("http.route", "hello");
 
@@ -49,23 +55,40 @@ public class ApiContextHandler extends ServletContextHandler
       } catch (InterruptedException e) {
       }
 
-      // start a child span
+      // Start a child span to define a new operation. You don't need to make a 
+      // lot of tiny child spans. A good rule of thumb is one for the controller, 
+      // one for the model, one for the database call, one for rendering, etc.
       Span childSpan = tracer.spanBuilder("my-server-span").startSpan();
+      
+      // Once you've created a child span, you can set it as the current span by 
+      // creating a closure (called a Scope in OpenTelemetry).
+      // This is a best practice: you always want the span to wrap the operation it
+      // is observing, and be available as the current span. Make sure to end the 
+      // span in the finally block.
+      // 
+      // This is clearly a lot of code. I recommend making your framework manage 
+      // spans for you, rather than adding spans directly in your application code.
+      // You can also use the @WithSpan annotation to easily wrap a method in a span.
       try (Scope scope = childSpan.makeCurrent()) {
         
-        // inside the new scope, getCurrentSpan returns childSpan.
-        // note that span methods can be chained.
-        Span.current().setAttribute("ProjectId", "456");
+        // Inside the new scope, getCurrentSpan now returns the childSpan.
+        // Also note that span methods can be chained.
+        Span.current().setAttribute("ProjectId", "456")
+                      .setAttribute("AccountId", "abcd");
           
 
-        // recordException automatically formats an exception event
-        childSpan.recordException(new RuntimeException("oops"));
+        // Exceptions are important for observing your system.
+        // The recordException method automatically formats an exception and 
+        // records it as an event on the span. See more about events below.
+        Span.current().recordException(new RuntimeException("oops"));
         
-        // in order for an exception to counts as an error,
-        // the status on the span must be to Error
-        childSpan.setStatus(StatusCode.ERROR);
+        // Not all exceptions are errors that fail the entire operation. For 
+        // example, it might be normal in some cases to try to open a file that does not exist. 
+        // To make an exception to count as an error, set the status on the span to Error.
+        // This will cause the span to create alerts and count against your error budget.
+        Span.current().setStatus(StatusCode.ERROR);
         
-        // pretend to do work
+        // Pretend to do work
         try {
           Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -73,15 +96,17 @@ public class ApiContextHandler extends ServletContextHandler
         
         try (PrintWriter writer = res.getWriter()) {
           
-          // events are structured logs, contextualized by the trace.
-          childSpan.addEvent("writing response",
-          Attributes.of(AttributeKey.stringKey("content"), "hello world"));
+          // Spans include events, which are structured logs. This is better than 
+          // regular logging, as the events are included as part of the trace, 
+          // allowing you to easily find all of the logs in a transaction.
+          Span.current().addEvent("writing response",
+            Attributes.of(AttributeKey.stringKey("content"), "hello world"));
           
           writer.write("Hello World");
         }
 
       } finally {
-        // make sure to close the span
+        // Always make sure to end spans that you create, or you will have a leak.
         childSpan.end();
       }
     }
